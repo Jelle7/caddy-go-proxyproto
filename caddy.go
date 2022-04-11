155cab321459e4eac20b91b72e6e08d6042c762a
Jelle Sturm <jelle.sturm@gmail.com>
2022-04-11 15:11:09 +0200

    Add Caddyfile support

--- caddy.go
+++ caddy.go
@@ -12,3 +12,30 @@ func (Wrapper) CaddyModule() caddy.ModuleInfo {
 		New: func() caddy.Module { return new(Wrapper) },
 	}
 }
+
+func (w *Wrapper) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	for d.Next() {
+		// No same-line options are supported
+		if d.NextArg() {
+			return d.ArgErr()
+		}
+
+		for d.NextBlock(0) {
+			switch d.Val() {
+			case "timeout":
+				if !d.NextArg() {
+					return d.ArgErr()
+				}
+				dur, err := caddy.ParseDuration(d.Val())
+				if err != nil {
+					return d.Errf("parsing proxy_protocol timeout duration: %v", err)
+				}
+				w.Timeout = caddy.Duration(dur)
+
+			default:
+				return d.ArgErr()
+			}
+		}
+	}
+	return nil
+}
